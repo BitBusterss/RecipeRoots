516e417a256a9b0aa4aced96d7d7f298efb973ef
Disha-shr <ds9192090@gmail.com>
2025-02-08 00:12:24 +0530

    newfeaturesadd

--- Frontend/src/pages/EditRecipe.jsx
+++ Frontend/src/pages/EditRecipe.jsx
@@ -71,7 +71,7 @@ export default function EditRecipe() {
             },
         });
         console.log("upload success:",response.data);
-        navigate("/");
+        navigate("/myRecipe");
     }
     catch(error){
         console.error("Error uploading recipe:",error.response?.data||error.message)
